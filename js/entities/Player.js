c2d90f0f792bcc6a4e317a0bb08e19413b41a7ee
khalil <rahmounimohamedkhalil@gmail.com>
2026-02-05 16:05:28 +0100

    feat: Know we can jum and the canvas take all the screen

--- js/entities/Player.js
+++ js/entities/Player.js
@@ -1,24 +1,60 @@
 export default class Player {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.w = 40;
-        this.h = 60;
-        this.vy = 0;
-        this.onGround = true;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+
+    this.w = 40;
+    this.h = 60;
+
+    this.color = "dodgerblue";
+
+    // Mouvement
+    this.speed = 200;
+
+    // Saut / physique
+    this.vy = 0;
+    this.gravity = 1200;
+    this.jumpForce = -450;
+    this.onGround = true;
+
+    // Sol
+    this.groundY = y;
+  }
+
+  update(dt, input) {
+    // Gauche / droite
+    if (input.isDown("ArrowRight")) {
+      this.x += this.speed * dt;
+    }
+    if (input.isDown("ArrowLeft")) {
+      this.x -= this.speed * dt;
     }
 
-    update(dt, input) {
-        if (input.isDown("ArrowRight")) {
-            this.x += 200 * dt;
-        }
-        if (input.isDown("ArrowLeft")) {
-            this.x -= 200 * dt;
-        }
+    // ⬆️ SAUT
+    if (input.isDown("ArrowUp") && this.onGround) {
+      this.vy = this.jumpForce;
+      this.onGround = false;
     }
 
-    draw(ctx) {
-        ctx.fillStyle = "dodgerblue";
-        ctx.fillRect(this.x, this.y, this.w, this.h);
+    // Gravité
+    this.vy += this.gravity * dt;
+    this.y += this.vy * dt;
+
+    // Collision sol
+    if (this.y >= this.groundY) {
+      this.y = this.groundY;
+      this.vy = 0;
+      this.onGround = true;
     }
+  }
+
+  setColor(color) {
+    this.color = color;
+    }
+
+
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  }
 }
